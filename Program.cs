579acacd17c764e13e28c96d81f3f908489dee19
KAJIYA Takeshi <kajiya.takeshi.e2s@jp.nssol.nipponsteel.com>
2023-10-11 04:34:10 +0000

    Second commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Presentation;
+using Drawing = DocumentFormat.OpenXml.Drawing;
+
+if (args.Length < 3)
+{
+  Console.Error.WriteLine("pptx-replacer [filename] [old-text] [new-text]");
+  return;
+}
+
+string filename = args[0];
+string oldText = args[1];
+string newText = args[2];
+
+if (File.Exists(filename))
+{
+  ReplacePptxFileText(filename, oldText, newText);
+}
+else
+{
+  Console.Error.WriteLine("File not exists: {0}", filename);
+}
+
+void ReplacePptxFileText(string filename, string oldText, string newText)
+{
+  using (PresentationDocument? presentationDocument = PresentationDocument.Open(filename, true))
+  {
+    PresentationPart? presentationPart = presentationDocument.PresentationPart;
+    if (presentationPart?.Presentation?.SlideIdList != null)
+    {
+      foreach (SlideId slideId in presentationPart.Presentation.SlideIdList.Cast<SlideId>())
+      {
+        string? relationshipId = slideId?.RelationshipId;
+        if (!string.IsNullOrEmpty(relationshipId))
+        {
+          SlidePart slidePart = (SlidePart)presentationPart.GetPartById(relationshipId);
+          ReplaceSlideText(slidePart, oldText, newText);
+        }
+      }
+    }
+
+    presentationDocument.Save();
+  }
+}
+
+void ReplaceSlideText(SlidePart slidePart, string oldText, string newText)
+{
+  var paragraphTextList = new List<string>();
+
+  if (slidePart?.Slide == null) return;
+
+  foreach (Drawing.Paragraph paragraph in slidePart.Slide.Descendants<Drawing.Paragraph>())
+  {
+    foreach (Drawing.Run run in paragraph.Elements<Drawing.Run>())
+    {
+      if (run.Text != null)
+      {
+        string t = run.Text.InnerText;
+        if (t.Contains(oldText))
+        {
+          var newRunText = new Drawing.Text(t.Replace(oldText, newText));
+          run.ReplaceChild(newRunText, run.Text);
+        }
+      }
+    }
+  }
+}
